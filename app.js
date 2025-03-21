1d8ce56528f1340f1e4eb12f0fea3e51decd2faa
wanghyunjooooo <w2324@e-mirim.hs.kr>
2025-03-21 11:53:44 +0900

    json으로 요청

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ const express = require('express');
 // Express 애플리케이션 생성
 const app = express();
 
+app.use(express.json());
 // 기본 라우트 설정
 app.get('/swag', (req, res) => {
   res.send('get swag');
 });
 
 app.post('/swag', (req, res) => {
-  res.send('post swag');
+  res.send(req.body);
 });
 
 // 서버가 3000번 포트에서 실행되도록 설정
@@ -18,3 +19,4 @@ const PORT = 2007;
 app.listen(PORT, () => {  
   console.log(`서버가 http://localhost:${PORT} 에서 실행 중입니다.`);
 });
+
