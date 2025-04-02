e2965fe73662ee4fd4847f448498f39d36b328c6
wanghyunjooooo <w2324@e-mirim.hs.kr>
2025-04-02 09:31:31 +0900

    mysql 연결

--- routes/app2.js
+++ routes/app2.js
@@ -1,20 +1,38 @@
-    const express = require('express');
-    const path = require('path');
+const express = require('express');
+const path = require('path');
+const mysql = require('mysql2');
 
-    const app = express();
-    const port = 5000;
-    app.set('view engine', 'ejs');
+const app = express();
+const port = 2007;
 
-    //__dirname : 현재 파일이 속해 있는 디렉토리의 절대경로
-    //path.foin : 운영체제에 맞추어 경로지정자를 설정해준다
-    app.set('views', path.join(__dirname, 'views'));
 
-    const travelList = ['뉴욕','파리','우리집','도쿄'];
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'mirim4',
+    database: 'traveldb'
+});
 
-    app.get('/travel',(req,res)=>{
-        res.render('travel',{travelList});
-    });
+db.connect(err => {
+    if(err){
+        console.error('mysql 연결 실패 :',err);
+        return;
+    }
+    console.log('musql에 연결 성공');
+})
 
-    app.listen(port, () => {
-        console.log(`서버가 http://localhost:${port} 에서 실행 중`);
-    });
+app.set('view engine', 'ejs');
+
+//__dirname : 현재 파일이 속해 있는 디렉토리의 절대경로
+//path.foin : 운영체제에 맞추어 경로지정자를 설정해준다
+app.set('views', path.join(__dirname, 'views'));
+
+const travelList = ['뉴욕', '파리', '우리집', '도쿄'];
+
+app.get('/travel', (req, res) => {
+    res.render('travel', { travelList });
+});
+
+app.listen(port, () => {
+    console.log(`서버가 http://localhost:${port} 에서 실행 중`);
+});
