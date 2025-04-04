7adefe499d610c447eb81121a500bbed2821d0c0
wanghyunjooooo <w2324@e-mirim.hs.kr>
2025-04-04 11:44:18 +0900

    Read(Get) 완성

--- app2.js
+++ app2.js
@@ -15,7 +15,7 @@ const db = mysql.createConnection({
 });
 
 db.connect(err => {
-    if(err){
+    if (err) {
         console.error('MySQL 연결 실패:', err);
         return;
     }
@@ -25,10 +25,26 @@ db.connect(err => {
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+// 여행 목록 조회
 app.get('/travel', async (req, res) => {
     try {
         const [results] = await db.promise().query('SELECT * FROM travellist');
         res.render('travel', { travelList: results });
+    } catch (err) {
+        console.error('데이터베이스 쿼리 실패', err);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+// 특정 여행 상세 정보 조회
+app.get('/travel/:id', async (req, res) => {
+    const travelId = req.params.id;
+    try {
+        const [results] = await db.promise().query('SELECT * FROM travellist WHERE id = ?', [travelId]);
+        if (results.length === 0) {
+            return res.status(404).send('여행 정보를 찾을 수 없습니다.');
+        }
+        res.render('travelDetail', { travel: results[0] });
     } catch (err) {
         console.error('데이터베이스 쿼리 실패', err);
         res.status(500).send('Internal Server Error');
